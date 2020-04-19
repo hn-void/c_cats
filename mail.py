81512da0c30f45e5e35b633a88a036cec6e5f699
ci11y <panic4thuy@gmail.com>
2020-04-18 17:32:40 -0700

    :sparkles: Add mail funcs

---
+++ mail.py
@@ -0,0 +1,28 @@
+import smtplib
+import sys
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+
+def create_mail(from_addr, to_addr, subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Date'] = formatdate()
+    return msg
+
+
+def send_mail(from_addr, password, to_addr, msg):
+    try:
+        # Login
+        smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+        smtpobj.ehlp()
+        smtpobj.starttls()
+        smtpobj.ehlp()
+        smtpobj.login(from_addr, password)
+        # Send
+        smtpobj.sendmail(from_addr, to_addr, msg.as_string())
+        smtpobj.close()
+    except Exception:
+        print(sys.exc_info())
