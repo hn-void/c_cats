171cc493950bb5e5b94a9457b05503f2b777bb8f
ci11y <panic4thuy@gmail.com>
2020-04-25 20:23:34 -0700

    :hammer: Fix logic for buying and selling

--- c_cats/btc_technical.py
+++ c_cats/btc_technical.py
@@ -30,10 +30,13 @@ Edit here, make signals
 """""""""
 ima_buy, ima_sell = technical.increasing_mavg(btc_df, short_term=1, long_term=20, band_width=0.01)
 bband_buy, bband_sell = technical.bollinger_band(btc_df, term=20, band_width=2)
+macd_buy, macd_sell = technical.ma_convergence_divergence(btc_df, short_term=12, long_term=26)
+est_buy, est_sell = technical.ma_estrangement(btc_df, term=25, band_width=0.1)
+cci_buy, cci_sell = technical.commodity_channel_index(btc_df, term=21, follower=100, contrarian=200)
+rsi_buy, rsi_sell = technical.relative_strength_index(btc_df, term=14, follower=20, contrarian=40)
 
-signal_buy = technical.normalize(ima_buy + bband_buy)
-signal_sell = technical.normalize(ima_sell + bband_sell)
-signal = signal_buy - signal_sell
+signal = ima_buy + bband_buy + macd_buy + est_buy + cci_buy + rsi_buy
+signal = signal - ima_sell - bband_sell - macd_sell - est_sell - cci_sell - rsi_sell
 """""""""
 Edit here
 """""""""
@@ -41,8 +44,8 @@ Edit here
 signal_today = signal.iloc[-1].values[0]
 print(signal_today)
 if signal_today > 0:
-    print('[c_cats]\tYou had better sell', signal_today, 'BTC today')
+    print('[c_cats]\tYou had better buy', signal_today, 'BTC today')
 elif signal_today < 0:
-    print('[c_cats]\tYou had better buy', math.fabs(signal_today), 'BTC today')
+    print('[c_cats]\tYou had better sell', math.fabs(signal_today), 'BTC today')
 else:
     print('[c_cats] You don\'t have to trade BTC today')
