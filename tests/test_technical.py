a3420e63af18dbdbcc23322eeb7b163e7fafc8bb
ci11y <panic4thuy@gmail.com>
2020-04-29 13:39:38 -0700

    :white_check_mark: Add a test for Bollinger band

--- tests/test_technical.py
+++ tests/test_technical.py
@@ -13,8 +13,16 @@ class TestTechnicalIndicators(TestCase):
 
     def test_increasing_mavg(self):
         ima_buy, ima_sell = technical.increasing_mavg(self.test_btc_df, short_term=3, long_term=7, band_width=0.01)
-        expected = pd.read_csv(os.path.dirname(__file__)+'/test_data/cal_increasing_mavg.csv')
+        expected = pd.read_csv(os.path.dirname(__file__)+'/test_data/expected_data/cal_increasing_mavg.csv')
         expected_buy = expected['buy_signal'].fillna(0.0)
         expected_sell = expected['sell_signal'].fillna(0.0)
         self.assertTrue(ima_buy['close'].equals(expected_buy))
         self.assertTrue(ima_sell['close'].equals(expected_sell))
+
+    def test_bollinger_band(self):
+        bband_buy, bband_sell = technical.bollinger_band(self.test_btc_df, term=7, coefficient=1)
+        expected = pd.read_csv(os.path.dirname(__file__)+'/test_data/expected_data/cal_bollinger_band.csv')
+        expected_buy = expected['buy_signal'].fillna(0.0)
+        expected_sell = expected['sell_signal'].fillna(0.0)
+        self.assertTrue(bband_buy['close'].equals(expected_buy))
+        self.assertTrue(bband_sell['close'].equals(expected_sell))
