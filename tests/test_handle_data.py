86e03980d015e6ef3b405c7ba8a3faf762ad4eda
ci11y <panic4thuy@gmail.com>
2020-04-28 19:01:26 -0700

    :art: Rename test_btc_data.json -> test_btc_latest.json

--- tests/test_handle_data.py
+++ tests/test_handle_data.py
@@ -7,7 +7,7 @@ from c_cats.btc_utils.handle_data import read_latest_btc
 class TestReadLatestBTC(TestCase):
 
     def setUp(self):
-        self.test_data = read_latest_btc(os.path.dirname(__file__)+'/test_data/test_btc_data.json', 'BTC')
+        self.test_data = read_latest_btc(os.path.dirname(__file__)+'/test_data/test_btc_latest.json', 'BTC')
 
     def test_read_latest_btc(self):
         self.assertEqual(self.test_data['id'], 1)
