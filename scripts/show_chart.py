26dd5c05614a3f5d2055de2ccadf850fa559e1ee
ci11y <panic4thuy@gmail.com>
2020-12-12 20:25:37 -0800

    :sparkles: Add moving average

--- scripts/show_chart.py
+++ scripts/show_chart.py
@@ -6,13 +6,15 @@ from matplotlib import pyplot as plt
 import technical
 
 
+plt.style.use('ggplot')
 FILE_NAME = 'btc2usd.csv'
 
 
 class TechnicalIndicator:
 
-    def __init__(self, df, color=None):
-        self.df = df.copy()
+    def __init__(self, df=None, name=None, color=None):
+        self.df = df
+        self.name = name
         self.color = color
 
 
@@ -20,14 +22,16 @@ class MavgIndicator(TechnicalIndicator):
 
     def __init__(self, df, term=26, color=None):
         self.df = technical.mavg(df=df, term=term)
+        self.name = 'Moving Average: ' + 'term='+str(term)
         self.color = color
 
 
 def show_chart(list_indicators, start_date=None, end_date=None):
     for indicator in list_indicators:
-        plt.plot(indicator.df['timestamp'], indicator.df['close'])
+        plt.plot(indicator.df['timestamp'], indicator.df['close'], label=indicator.name, color=indicator.color)
     plt.xlabel('Date')
     plt.ylabel('USD/BTC')
+    plt.legend()
     plt.show()
 
 
@@ -37,7 +41,7 @@ if __name__ == '__main__':
 
     df_usd2btc = pd.read_csv(FILE_NAME)
     df_usd2btc['timestamp'] = df_usd2btc['time'].apply(lambda x: datetime.datetime.fromtimestamp(x))
-    raw_indicator = TechnicalIndicator(df=df_usd2btc)
+    raw_indicator = TechnicalIndicator(df=df_usd2btc[['close', 'timestamp']], name='Raw Data')
     list_indicators.append(raw_indicator)
 
     mavg_indicator = MavgIndicator(df=raw_indicator.df)
